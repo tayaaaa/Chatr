02d0e1f4609c132f6c6659c922f0380aa064b37a
thomassmith <thomassmith@Thomass-MacBook-Pro.local>
2019-07-26 10:38:09 +1000

    created Lesson model

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,5 +1,5 @@
 class Lesson < ApplicationRecord
   has_many :userbookings
   has_many :reviews
-  belongs_to :teacher, :class_name => “User”
+  belongs_to :teacher, :class_name => :user, :foreign_key => :user_id
 end
