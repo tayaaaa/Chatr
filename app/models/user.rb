3c435681a3cde18464164243976e7f1922e1161f
tayaaaa <lacey.taya@gmail.com>
2019-07-26 11:50:21 +1000

    Added relationships into the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :conversations
+	has_one :role
+	has_many :notifications
+	has_one :profile
+	has_many :messages
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
