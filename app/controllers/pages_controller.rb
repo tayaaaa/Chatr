2860bfe465b532956c30b1a151eeee29ae033e0e
tayaaaa <lacey.taya@gmail.com>
2019-07-29 17:16:50 +1000

    add lesson card details

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,21 @@
 class PagesController < ApplicationController
+    before_action :set_lessons
+
     def index
+        @lesson_card_array = []
+        @lessons.each do |lesson|
+            lesson_card_info = {
+                language: lesson.languageskill.language_name, 
+                duration: lesson.duration, 
+                maxbooking: lesson.maxbooking, 
+                description: lesson.description, teacher_name:lesson.user.profile.firstname, teacher_id:lesson.user_id,
+                price:lesson.price}
+            @lesson_card_array << lesson_card_info
+        end
+    end
+
+    private
+    def set_lessons
+        @lessons = Lesson.all
     end
 end
