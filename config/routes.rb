25d29dd4bcc9a4b6c5c583123f84a414fabfbeb3
tayaaaa <lacey.taya@gmail.com>
2019-07-31 13:04:19 +1000

    functionality and card styling on bookings page

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   resources :profiles
   resources :notifications
   devise_for :users
+  put '/userbookings/completelesson/:id', to: 'userbookings#completelesson'
   root to: 'pages#landing'
   get '/browse', to: 'pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
