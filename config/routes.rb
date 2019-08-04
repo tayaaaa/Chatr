0968733717b21fc0b79249a914e9942305394a2c
thomassmith <thomassmith@Thomass-MBP.gateway>
2019-08-04 22:54:33 +1000

    worked on reviews

--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,6 @@ Rails.application.routes.draw do
   get '/browse', to: 'pages#index'
   get '/browse/lessons', to: 'pages#browse_lessons'
   get '/browse/teachers', to: 'pages#browse_teachers'
+  get '/userbookings/:id/review', to: 'reviews#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
