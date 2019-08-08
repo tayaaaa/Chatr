4afe455fefc4e167f9c56ca1fd7391802e79870a
tayaaaa <lacey.taya@gmail.com>
2019-08-08 11:40:30 +1000

    deleted unncessary column from messages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_06_043009) do
+ActiveRecord::Schema.define(version: 2019_08_08_013702) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,7 +71,6 @@ ActiveRecord::Schema.define(version: 2019_08_06_043009) do
   create_table "messages", force: :cascade do |t|
     t.bigint "conversation_id"
     t.text "content"
-    t.datetime "date_sent"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "author_id"
