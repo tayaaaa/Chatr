fd9fc21acf88380f732434743f44c90617c328a4
tayaaaa <lacey.taya@gmail.com>
2019-08-08 15:29:26 +1000

    bookings, replies and reviews with pundit

---
+++ app/policies/review_policy.rb
@@ -0,0 +1,49 @@
+class ReviewPolicy < ApplicationPolicy
+    attr_reader :user, :review
+  
+    def initialize(user, review)
+      @user = user
+      @review = review
+    end
+  
+    def index?
+        false
+    end
+  
+    def show?
+        false
+    end
+  
+    def create?
+        new?
+    end
+  
+    def new?
+      false
+    end
+  
+    def update?
+    end
+  
+    def edit?
+        false
+    end
+  
+    def destroy?
+        false
+    end
+  
+    # class Scope
+    #   attr_reader :user, :scope
+  
+    #   def initialize(user, scope)
+    #     @user = user
+    #     @scope = scope
+    #   end
+  
+    #   def resolve
+    #     scope.all
+    #   end
+    # end
+end
+  
